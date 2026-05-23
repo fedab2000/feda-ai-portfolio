f0b14342c2b46e784161e495646b32b41d65705b
Feda Bashbishi <fbashbis@uwaterloo.ca>
2026-05-23 15:26:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ with col1:
 
     st.link_button(
         "Open App",
-        https://insurance-pricing-ai-myxjhrl3vaeyahw8kkrmc6.streamlit_app.app
+        https://insurance-pricing-ai-myxjhrl3vaeyahw8kkrmc6.streamlit_app.py
     )
 
 with col2:
